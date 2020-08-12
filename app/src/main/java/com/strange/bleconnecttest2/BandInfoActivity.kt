edc01a2a7ba4e8217daf05c0913ecbd402e23f39
LeeSangHa <38284431+StrangeLee@users.noreply.github.com>
2020-08-12 10:51:21 +0900

    [Add] Clipboard 기능
    Row Data TextView를 클릭시 Clipboard에 복사하는 기능 추가
    이동거리 소수점 2자리 까지 보이게 표시

--- app/src/main/java/com/strange/bleconnecttest2/BandInfoActivity.kt
+++ app/src/main/java/com/strange/bleconnecttest2/BandInfoActivity.kt
@@ -1,5 +1,8 @@
 package com.strange.bleconnecttest2
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
@@ -9,8 +12,9 @@ import androidx.databinding.DataBindingUtil
 import com.strange.bleconnecttest2.data.AdvertisingData
 import com.strange.bleconnecttest2.databinding.ActivityBandInfoBinding
 import kotlinx.android.synthetic.main.activity_band_info.view.*
+import org.jetbrains.anko.toast
 
-class BandInfoActivity() : AppCompatActivity(){
+class BandInfoActivity : AppCompatActivity(){
 
     private lateinit var binding: ActivityBandInfoBinding
     private lateinit var scanResult: ByteArray
@@ -24,6 +28,14 @@ class BandInfoActivity() : AppCompatActivity(){
 
         // xml 파일에 data 넘겨주는 과정
         binding.advertising = getAdvertisingData(scanResult)
+
+        // row data text view 클릭시 clipboard 에 복사
+        binding.root.tv_rowdata.setOnClickListener {
+            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip : ClipData = ClipData.newPlainText("Row data를 복사했습니다.", binding.root.tv_rowdata.text.toString())
+            clipboard.primaryClip = clip
+            toast("Row Data값을 복사했습니다.")
+        }
 
         printAdvertisingData(scanResult)
     }
@@ -52,9 +64,9 @@ class BandInfoActivity() : AppCompatActivity(){
         }
 
         Log.d("Data", "HRM : ${msg.substring(22, 24).toInt(16)}")
-        Log.d("Data", "Step : ${msg.substring(24, 28).toInt(16).toString()}") // 16진수 변환 후 다시 10진수 String 으로 변환
+        Log.d("Data", "Step : ${msg.substring(24, 28).toInt(16)}") // 16진수 변환 후 다시 10진수 String 으로 변환
         Log.d("Data", "Calorie : ${msg.substring(28, 32)}")
-        Log.d("Data", "distance : ${msg.substring(32, 36).toInt(16).toString().toInt() / 100}")
+        Log.d("Data", "distance : ${(msg.substring(32, 36).toInt(16).toString().toInt() / 100).toFloat()}")
         Log.d("Data", "sleep status : ${msg.substring(36, 38).toInt(16)}")
         Log.d("Data", "hands off : ${msg.substring(38, 40).toInt(16)}")
         Log.d("Data", "sleep time : ${msg.substring(40, 44).toInt(16)}")
@@ -97,7 +109,7 @@ class BandInfoActivity() : AppCompatActivity(){
             "심박수 : " + msg.substring(22, 24).toInt(16).toString(),
             "걸음수 : " + msg.substring(24, 28).toInt(16).toString(),
             "칼로리 : " + msg.substring(28, 32).toInt(16).toString() + "Kcal",
-            "이동거리 : " + (msg.substring(32, 36).toInt(16).toString().toInt() / 100).toString() + "km",
+            "이동거리 : " + String.format("%.2f", (msg.substring(32, 36).toInt(16).toString().toInt() / 100).toFloat()) + "km",
             "수면 상태 : " + msg.substring(36, 38).toInt(16).toString(),
             "착용상태 : $handsOff",
             "수면시간 : " + msg.substring(40, 44).toInt(16).toString(),
